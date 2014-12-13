c5bd8ab06698b67756b578e9272988ab91422f09
Steve Phillips <steve@tryingtobeawesome.com>
2014-12-13 05:19:31 -0800

    Added err_test.go, TestFirstError

---
+++ err_test.go
@@ -0,0 +1,17 @@
+// Steve Phillips / elimisteve
+// 2014.12.13
+
+package help
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestFirstError(t *testing.T) {
+	var e1, e2, e3 error = errors.New("e1"), nil, errors.New("e3")
+	err := FirstError(e1, e2, e3)
+	if err != e1 {
+		t.Errorf("Got %v, wanted %v", err, e1)
+	}
+}
