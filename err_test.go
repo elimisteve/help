c922fbaa4d778a014ccd8674392162c72f992d4e
Steve Phillips <steve@tryingtobeawesome.com>
2014-12-13 05:20:49 -0800

    Trivially improved TestFirstError

--- err_test.go
+++ err_test.go
@@ -12,6 +12,6 @@ func TestFirstError(t *testing.T) {
 	var e1, e2, e3 error = errors.New("e1"), nil, errors.New("e3")
 	err := FirstError(e1, e2, e3)
 	if err != e1 {
-		t.Errorf("Got %v, wanted %v", err, e1)
+		t.Errorf("Got `%v`, wanted `%v`", err, e1)
 	}
 }
